416c2f97d3b72fdc20964f35151ae609e2dac744
Dan.C <dan.c94@outlook.com>
2022-09-14 11:06:25 -0300

    js testings

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-// parallx o loader
-// abrir un producto y que llame el archivo en JS
+
+var click = document.getElementById("btn1")
+
+
+function show(){
+  console.log('nenene')
+  // document.getElementById('menu_file').innerHTML=`
+  //  <iframe src="menus/full-menu.pdf#toolbar=0" >
+  //  </iframe>`
+}
+
+////////////////////
+
+
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+// var btn4 = document.getElementById('btn4');
+// var menuSelection = 0;
+// btn1.onclick = 
+//   innerHTML=
+//    <iframe src="menus/full-menu.pdf#toolbar=0">
+//    </iframe>
+
+
+//  if (btn1.onclick) {
+//   document.getElementById('menu_file').innerHTML=(
+//    <iframe src="menus/full-menu.pdf#toolbar=0" >
+//    </iframe>)
+//  }
+//  else if (menuSelection == "2") {
+//   document.getElementById('menu_file').innerHTML=
+//    <iframe src="menus/full-menu.pdf#toolbar=0" >
+//    </iframe>
+// }else if (menuSelection == "3") {
+//   document.getElementById('menu_file').innerHTML=
+//    <iframe src="menus/full-menu.pdf#toolbar=0" >
+//    </iframe>
+// } else (menuSelection == "4") ;{
+//   document.getElementById('menu_file').innerHTML=
+//    <iframe src="menus/full-menu.pdf#toolbar=0" >
+//    </iframe>
+//    }
