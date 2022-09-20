f0766a3beb579c4289fc2515be8b3ae669c48fa5
Dan.C <dan.c94@outlook.com>
2022-09-19 23:21:00 -0300

    pre update

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+const $drowdownArrow = document.querySelector('.ri-menu-fold-line');
+const $checkbox = document.getElementById('openDropdown');
+const $dropdownMenu = document.querySelector('.dropdown-menu');
 
-var click = document.getElementById("btn1")
+$checkbox.addEventListener('change', () => {
+  $drowdownArrow.classList.toggle('rotate-dropdown-arrow');
+});
 
-
-function show(){
-  console.log('nenene')
-  // document.getElementById('menu_file').innerHTML=`
-  //  <iframe src="menus/full-menu.pdf#toolbar=0" >
-  //  </iframe>`
-}
+$dropdownMenu.addEventListener('click', (e) => {
+  $checkbox.checked = false;
+  // setting checked to false won't trigger 'change'
+  // event, manually dispatch an event to rotate
+  // dropdown arrow icon
+  $checkbox.dispatchEvent(new Event('change'));
+});
 
 ////////////////////
 
