1b4804c531159fdc09f6d07dd6762df13be444e5
Dan.C <dan.c94@outlook.com>
2022-09-20 10:55:20 -0300

    e

--- main.js
+++ main.js
@@ -8,40 +8,5 @@ $checkbox.addEventListener('change', () => {
 
 $dropdownMenu.addEventListener('click', (e) => {
   $checkbox.checked = true;
-  // setting checked to false won't trigger 'change'
-  // event, manually dispatch an event to rotate
-  // dropdown arrow icon
   $checkbox.dispatchEvent(new Event('change'));
 });
-
-////////////////////
-
-
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-// var btn4 = document.getElementById('btn4');
-// var menuSelection = 0;
-// btn1.onclick = 
-//   innerHTML=
-//    <iframe src="menus/full-menu.pdf#toolbar=0">
-//    </iframe>
-
-
-//  if (btn1.onclick) {
-//   document.getElementById('menu_file').innerHTML=(
-//    <iframe src="menus/full-menu.pdf#toolbar=0" >
-//    </iframe>)
-//  }
-//  else if (menuSelection == "2") {
-//   document.getElementById('menu_file').innerHTML=
-//    <iframe src="menus/full-menu.pdf#toolbar=0" >
-//    </iframe>
-// }else if (menuSelection == "3") {
-//   document.getElementById('menu_file').innerHTML=
-//    <iframe src="menus/full-menu.pdf#toolbar=0" >
-//    </iframe>
-// } else (menuSelection == "4") ;{
-//   document.getElementById('menu_file').innerHTML=
-//    <iframe src="menus/full-menu.pdf#toolbar=0" >
-//    </iframe>
-//    }
